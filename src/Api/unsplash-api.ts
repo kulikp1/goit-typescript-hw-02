18b2b54eca245b76a88707381372efc358f18784
kulikp1 <pashakylik2@gmail.com>
2024-07-13 11:21:47 +0300

    Update unsplash-api.ts

--- src/Api/unsplash-api.ts
+++ src/Api/unsplash-api.ts
@@ -1,19 +1,31 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { FetchImages } from './unsplash-api.types';
 
 const API_KEY = 'jjCRPzrxadrk_T1R5NACzD0KTZUZsKPkF_iarTpTaMU';
 const BASE_URL = `https://api.unsplash.com/search/photos/`;
 
+interface UnsplashImage {
+  id: string;
+  urls: {
+    regular: string;
+  };
+}
+
+interface FetchImagesResponse {
+  results: UnsplashImage[];
+}
+
 const options = {
   headers: { Authorization: `Client-ID ${API_KEY}` },
 };
 
-export const fetchImages: FetchImages = async (query: string, page: number) => {
-  const searchParams: Record<string, any> = new URLSearchParams({
+export const fetchImages: FetchImages = async (query: string, page: number): Promise<UnsplashImage[]> => {
+  const searchParams = new URLSearchParams({
     query,
     page: page.toString(),
     per_page: '20',
   });
 
-  return (await axios(`${BASE_URL}?${searchParams}`, options)).data.results;
+  const response: AxiosResponse<FetchImagesResponse> = await axios.get(`${BASE_URL}?${searchParams}`, options);
+  return response.data.results;
 };
